f67a26ddd6f5c802b741a399251da4fe98b578a5
emzar kviciani <ndoreuli25@gmail.com>
2025-11-09 00:14:41 +0400

    ragaca

--- exam_project/exam_app/models.py
+++ exam_project/exam_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+# მერვე დავალების პროექტს მივამატე ახალი
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.IntegerField()
+    is_alive = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.TextField()
+    pages = models.PositiveIntegerField()
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Publisher(models.Model):
+    company_name = models.CharField(max_length=100)
+    founded = models.DateTimeField()
+    employees = models.FloatField()
+
+    def __str__(self):
+        return self.company_name
